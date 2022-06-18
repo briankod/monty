d9115900fc6701e11aa7a55e485bcec1347db143
briankod <brianmugambi199@gmail.com>
2022-06-18 20:24:09 +0300

    C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -2,16 +2,17 @@
 #define _MONTY_H_
 
 #define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <errno.h>
-#include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
